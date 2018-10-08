4c11539010e4a3e168661c1fbaf77bd0323b080d
jakehilborn <jakehilborn@gmail.com>
2018-10-08 11:52:45 -0700

    startStopButton wired up

--- bpm-alert WatchKit Extension/InterfaceController.swift
+++ bpm-alert WatchKit Extension/InterfaceController.swift
@@ -2,9 +2,39 @@ import WatchKit
 import Foundation
 import HealthKit
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate {
     let healthStore = HKHealthStore()
-
+    var isRunning = false
+    
+    @IBOutlet var startStopButton: WKInterfaceButton!
+    @IBAction func startStopButtonAction() {
+        print("button action")
+        if (self.isRunning) {
+            print("stopping service")
+            self.isRunning = false
+            self.startStopButton.setTitle("start")
+        } else {
+            print("starting service")
+            self.isRunning = true
+            self.startStopButton.setTitle("stop")
+        }
+    }
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
+        switch toState {
+        case .running:
+            print("case running")
+        case .ended:
+            print("case ended")
+        default:
+            print("Unexpected state \(toState)")
+        }
+    }
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
+        print("Workout error")
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
 
